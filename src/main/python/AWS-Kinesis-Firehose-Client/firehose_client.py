46cf902307d15d33263dbde7cc6049a0fcef82e7
bs <23002942+berksudan@users.noreply.github.com>
2020-10-21 19:34:33 +0300

    small changes

--- src/main/python/AWS-Kinesis-Firehose-Client/firehose_client.py
+++ src/main/python/AWS-Kinesis-Firehose-Client/firehose_client.py
@@ -15,7 +15,7 @@ CSV_PARENT_DIR = "data"
 CSV_NAME = "loan.csv"
 
 
-def list_to_csv_line(row): ','.join(row) + '\n'
+def list_to_csv_line(row) -> str: return ','.join(row) + '\n'
 
 
 def create_aws_session(aws_access_key_id, aws_secret_access_key, aws_session_token, client_service_name='firehose'):
@@ -30,9 +30,9 @@ def extract_num_lines(file_path: str) -> int:
     return sum(1 for _ in open(file_path))
 
 
-def download_data(data_url:str, data_parent_dir: str) -> str:
+def download_data(data_url: str, data_parent_dir: str) -> str:
     data_name = data_url.split('/')[-1]
-    csv_target_path = Path(data_parent_dir,data_name)
+    csv_target_path = Path(data_parent_dir, data_name)
     if not os.path.isfile(csv_target_path):
         if not os.path.exists(data_parent_dir):
             os.makedirs(data_parent_dir)
@@ -45,8 +45,8 @@ def download_data(data_url:str, data_parent_dir: str) -> str:
 
 
 def main():
-    # if len(sys.argv) < 4:
-    #     exit("[ERROR] Arguments should be <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <AWS_SESSION_TOKEN>")
+    if len(sys.argv) < 4:
+        exit('[ERROR] Arguments should be "<AWS_ACCESS_KEY_ID>" "<AWS_SECRET_ACCESS_KEY>" "<AWS_SESSION_TOKEN>"')
     zip_full_path = download_data(data_url=DATA_URL, data_parent_dir=CSV_PARENT_DIR)
 
     loan_data_loader_firehose_client = create_aws_session(
@@ -55,20 +55,19 @@ def main():
         aws_session_token=sys.argv[3])
 
     records = []
-    print(zip_full_path)
     with zipfile.ZipFile(zip_full_path, 'r') as zip_ref:
         zip_ref.extractall(CSV_PARENT_DIR)
-    exit(445)
-    num_lines = extract_num_lines(zip_full_path)
-    with open(zip_full_path, 'r') as csv_file:
+    csv_full_path = str(Path(CSV_PARENT_DIR, CSV_NAME))
+    num_lines = extract_num_lines(csv_full_path)
+    with open(csv_full_path, 'r') as csv_file:
         loan_data_reader = csv.reader(csv_file)
 
         record_count = 1
         for loan_row in loan_data_reader:
             if record_count % 500 == 0:
-                # response = loanDataLoaderFirehoseClient.put_record_batch(
-                #     DeliveryStreamName=DELIVERY_STREAM_NAME,
-                #     Records=records)
+                loan_data_loader_firehose_client.put_record_batch(
+                    DeliveryStreamName=DELIVERY_STREAM_NAME,
+                    Records=records)
                 print('[INFO] Delta-Transferred:%d, Percentage:%%%.3f' % (record_count, 100 * record_count / num_lines))
                 records.clear()
             records.append({"Data": list_to_csv_line(loan_row)})
